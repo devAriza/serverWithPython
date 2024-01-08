24fee2f02a576330ad6571b17cf5678eb654ad7f
Jorge Ariza <v.ariza.0511@gmail.com>
2024-01-07 22:59:28 -0600

    Agregar validacion de contrasenias para que dos atributos sean iguales

--- validacion.py
+++ validacion.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel, validator, ValidationError
+from pydantic import BaseModel, ValidationError, field_validator, validator
 from typing import Optional
 
 class User(BaseModel):
     username: str #requerido
     password: str
+    repeat_password: str
     email: str
     age: Optional[int] = None
 
     #referencia a clase y atributo
     #decorador con parametro el atributo
-    @validator('username')
+    @field_validator('username')
     def username_validation_lenght(cls, username):
         if len(username) < 3:
             raise ValueError('La longitud minima es de 4 caracteres.')
@@ -18,11 +19,20 @@ class User(BaseModel):
             raise ValueError('La longitud maxima es de 50 caracteres.')
 
         return username
+
+    @validator('repeat_password')
+    def repeat_password_validation(cls, repeat_password, values):
+        if 'password' in values and repeat_password != values['password']:
+            raise ValueError('Las contrasenias son diferentes')
+
+        return repeat_password
         
 try:
+    
     user1 = User(
-        username = 'KK',
-        password = 'password123',
+        username = 'Jorge',
+        password = '123',
+        repeat_password = '123',
         email = 'info@codigofacilito.com',
         age = 10
         )
